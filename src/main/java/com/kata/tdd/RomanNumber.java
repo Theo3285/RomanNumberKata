8f94b420b27015e778b60dc123d8b58ccd039289
Christophe THEPAUT <christophe@thepaut.net>
2018-09-13 16:36:57 +0200

    first simplest test passed: 1 to I

---
+++ src/main/java/com/kata/tdd/RomanNumber.java
@@ -0,0 +1,7 @@
+package com.kata.tdd;
+
+public class RomanNumber {
+    public static String generate(int decimal) {
+        return "I";
+    }
+}
---
+++ src/test/java/com/kata/tdd/RomanNumberGeneratorShould.java
@@ -0,0 +1,15 @@
+package com.kata.tdd;
+
+import org.junit.Test;
+
+import static com.kata.tdd.RomanNumber.generate;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class RomanNumberGeneratorShould {
+
+    @Test
+    public void generate_roman_number_for_a_given_decimal() {
+        assertThat(generate(1), is("I"));
+    }
+}
