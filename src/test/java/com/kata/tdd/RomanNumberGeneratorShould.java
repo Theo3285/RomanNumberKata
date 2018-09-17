048ab644f2df590c889b76b3dfaf2e8f13b6fec8
Christophe THEPAUT <christophe@thepaut.net>
2018-09-17 11:39:48 +0200

    Completed mapping table and verified all conversion are passing

--- src/test/java/com/kata/tdd/RomanNumberGeneratorShould.java
+++ src/test/java/com/kata/tdd/RomanNumberGeneratorShould.java
@@ -21,5 +21,17 @@ public class RomanNumberGeneratorShould {
         assertThat(generate(10), is("X"));
         assertThat(generate(11), is("XI"));
         assertThat(generate(16), is("XVI"));
+        assertThat(generate(40), is("XL"));
+        assertThat(generate(50), is("L"));
+        assertThat(generate(60), is("LX"));
+        assertThat(generate(90), is("XC"));
+        assertThat(generate(100), is("C"));
+        assertThat(generate(116), is("CXVI"));
+        assertThat(generate(695), is("DCXCV"));
+        assertThat(generate(900), is("CM"));
+        assertThat(generate(1000), is("M"));
+        assertThat(generate(2008), is("MMVIII"));
+        assertThat(generate(1999), is("MCMXCIX"));
+        assertThat(generate(846), is("DCCCXLVI"));
     }
 }
