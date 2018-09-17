9a3222b11d8c249436bbd061b5a9c0bc8ad16e28
Christophe THEPAUT <christophe@thepaut.net>
2018-09-17 13:29:03 +0200

    finalize with extract class

--- src/main/java/com/kata/tdd/RomanNumber.java
+++ src/main/java/com/kata/tdd/RomanNumber.java
@@ -4,35 +4,11 @@ public class RomanNumber {
     public static String generate(int decimal) {
         StringBuilder roman = new StringBuilder();
         for (RomanToDecimal romanToDecimal : RomanToDecimal.values()) {
-            while (decimal >= romanToDecimal.decimal) {
-                roman.append(romanToDecimal.roman);
-                decimal -= romanToDecimal.decimal;
+            while (decimal >= romanToDecimal.decimal()) {
+                roman.append(romanToDecimal.roman());
+                decimal -= romanToDecimal.decimal();
             }
         }
         return roman.toString();
-    }
-
-    enum RomanToDecimal {
-        ONE_THOUSAND("M", 1000),
-        NINE_HUNDRED("CM", 900),
-        FIVE_HUNDRED("D", 500),
-        FOUR_HUNDRED("CD", 400),
-        HUNDRED("C", 100),
-        NINETY("XC", 90),
-        FIFTY("L", 50),
-        FORTY("XL", 40),
-        TEN("X", 10),
-        NINE("IX", 9),
-        FIVE("V", 5),
-        FOUR("IV", 4),
-        ONE("I", 1);
-
-        private final String roman;
-        private final int decimal;
-
-        RomanToDecimal(String roman, int decimal) {
-            this.roman = roman;
-            this.decimal = decimal;
-        }
     }
 }
---
+++ src/main/java/com/kata/tdd/RomanToDecimal.java
@@ -0,0 +1,34 @@
+package com.kata.tdd;
+
+enum RomanToDecimal {
+    ONE_THOUSAND("M", 1000),
+    NINE_HUNDRED("CM", 900),
+    FIVE_HUNDRED("D", 500),
+    FOUR_HUNDRED("CD", 400),
+    HUNDRED("C", 100),
+    NINETY("XC", 90),
+    FIFTY("L", 50),
+    FORTY("XL", 40),
+    TEN("X", 10),
+    NINE("IX", 9),
+    FIVE("V", 5),
+    FOUR("IV", 4),
+    ONE("I", 1);
+
+    private final String roman;
+    private final int decimal;
+
+    RomanToDecimal(String roman, int decimal) {
+        this.roman = roman;
+        this.decimal = decimal;
+    }
+
+    public String roman() {
+        return roman;
+    }
+
+    public int decimal() {
+        return decimal;
+    }
+
+}
